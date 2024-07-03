e707ffdabcab5e1742ec6c245b26e740f9418752
gn-t-k <gntk1995422@gmail.com>
2024-07-04 06:49:25 +0900

    03/03

--- exercises/03_ticket_v1/03_modules/src/lib.rs
+++ exercises/03_ticket_v1/03_modules/src/lib.rs
@@ -1,11 +1,4 @@
-mod helpers {
-    // TODO: Make this code compile, either by adding a `use` statement or by using
-    //  the appropriate path to refer to the `Ticket` struct.
-
-    fn create_todo_ticket(title: String, description: String) -> Ticket {
-        Ticket::new(title, description, "To-Do".into())
-    }
-}
+mod helpers;
 
 struct Ticket {
     title: String,
