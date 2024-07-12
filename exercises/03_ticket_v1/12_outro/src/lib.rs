c2372fbf9121d703fd8ac1c29d73f79bc4db7d60
Gentoku Murakami <54714779+gn-t-k@users.noreply.github.com>
2024-07-12 14:24:12 +0900

    2024/07/11 (#14)
    * 03/11
    
    * 03/12

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,75 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+        Self::assert_product_name(&product_name);
+        Self::assert_quantity(&quantity);
+        Self::assert_unit_price(&unit_price);
+
+        Order {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, new_name: String) {
+        Self::assert_product_name(&new_name);
+        self.product_name = new_name;
+    }
+
+    pub fn set_quantity(&mut self, new_quantity: u32) {
+        Self::assert_quantity(&new_quantity);
+        self.quantity = new_quantity;
+    }
+
+    pub fn set_unit_price(&mut self, new_unit_price: u32) {
+        Self::assert_unit_price(&new_unit_price);
+        self.unit_price = new_unit_price;
+    }
+
+    fn assert_product_name(product_name: &str) {
+        if product_name.is_empty() {
+            panic!("The product name can't be empty");
+        }
+        if product_name.len() > 300 {
+            panic!("The product name can't be longer than 300 bytes");
+        }
+    }
+
+    fn assert_quantity(quantity: &u32) {
+        if *quantity == 0 {
+            panic!("The quantity must be strictly greater than zero");
+        }
+    }
+
+    fn assert_unit_price(unit_price: &u32) {
+        if *unit_price == 0 {
+            panic!("The unit price must be strictly greater than zero");
+        }
+    }
+}
