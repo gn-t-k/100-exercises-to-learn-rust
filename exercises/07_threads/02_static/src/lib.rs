ac95f094b4939643538bdf3f44164799b2abcd80
gn-t-k <gntk1995422@gmail.com>
2024-09-11 15:46:30 +0900

    07/02

--- exercises/07_threads/02_static/src/lib.rs
+++ exercises/07_threads/02_static/src/lib.rs
@@ -4,7 +4,20 @@
 use std::thread;
 
 pub fn sum(slice: &'static [i32]) -> i32 {
-    todo!()
+    if slice.is_empty() {
+        return 0;
+    }
+
+    let mid = slice.len() / 2;
+    let (left, right) = slice.split_at(mid);
+
+    let left_handle = thread::spawn(move || left.iter().sum::<i32>());
+    let right_handle = thread::spawn(move || right.iter().sum::<i32>());
+
+    let left_sum = left_handle.join().unwrap();
+    let right_sum = right_handle.join().unwrap();
+
+    left_sum + right_sum
 }
 
 #[cfg(test)]
