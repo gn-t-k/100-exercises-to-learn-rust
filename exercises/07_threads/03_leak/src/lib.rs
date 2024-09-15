5bce78d066631872dc8132c6942b281d8eee41de
Gentoku Murakami <54714779+gn-t-k@users.noreply.github.com>
2024-09-15 18:34:20 +0900

    07/03 (#54)

--- exercises/07_threads/03_leak/src/lib.rs
+++ exercises/07_threads/03_leak/src/lib.rs
@@ -6,7 +6,23 @@
 use std::thread;
 
 pub fn sum(v: Vec<i32>) -> i32 {
-    todo!()
+    let v = v.leak();
+
+    if v.is_empty() {
+        return 0;
+    }
+
+    let mid = v.len() / 2;
+    let (left, right) = v.split_at(mid);
+
+    let (left_handle, right_handle) = (
+        thread::spawn(move || left.iter().sum::<i32>()),
+        thread::spawn(move || right.iter().sum::<i32>()),
+    );
+
+    let (left_sum, right_sum) = (left_handle.join().unwrap(), right_handle.join().unwrap());
+
+    left_sum + right_sum
 }
 
 #[cfg(test)]
